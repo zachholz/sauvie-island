8a4b6541ed4836c0055bcfb535ef0fada6460875
Shelby Clayton and Zach Holz <zach@>
2016-07-20 19:21:33 -0700

    Create click function for farm info, define fruit vars, basic dropdown form

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var columbiaFruit = ["apples", "oranges"];
+var douglasFruit = ["apples", "oranges"];
+var krugersFruit = ["apples", "oranges"];
+var pumpkinFruit = ["apples", "oranges"];
+var sauvieBlueberryFruit = ["apples", "oranges"];
+var sauvieFarmFruit = ["apples", "oranges"];
+var sauvieLavenderFruit = ["apples", "oranges"];
+
+
+
+$(document).ready(function() {
+  $(".farm-title").click(function() {
+    $(".farm-info").toggle();
+  });
+});
