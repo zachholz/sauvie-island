da80ff4486ef829618ce425a78fa6dba88e0131d
Shelby Clayton and Zach Holz <zach@>
2016-07-20 20:50:22 -0700

    Create farms object with each farm with fruits defined

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
-var columbiaFruit = ["apples", "oranges"];
-var douglasFruit = ["apples", "oranges"];
-var krugersFruit = ["apples", "oranges"];
-var pumpkinFruit = ["apples", "oranges"];
-var sauvieBlueberryFruit = ["apples", "oranges"];
-var sauvieFarmFruit = ["apples", "oranges"];
-var sauvieLavenderFruit = ["apples", "oranges"];
+var farms = {
+    names: [columbiaFruit, douglasFruit, krugersFruit, pumpkinFruit, sauvieFarmFruit, sauvieBlueberryFruit, sauvieLavenderFruit]
+}
+var columbiaFruit = {fruits: ["apples", "oranges"]};
+var douglasFruit = {fruits: ["apples", "oranges"]};
+var krugersFruit = {fruits: ["apples", "oranges"]};
+var pumpkinFruit = {fruits: ["apples", "oranges"]};
+var sauvieBlueberryFruit = {fruits: ["apples"]};
+var sauvieFarmFruit = {fruits: ["oranges"]};
+var sauvieLavenderFruit = {fruits: ["apples"]};
 
-
+// loop through farm names and find all those that contain fruit: apples
+var farmsWithApples = farms.map(function(name) {
+  this.names.forEach(function(fruit))
+})
 
 // user interface
 $(document).ready(function() {
   $("form#searchable").submit(function(event) {
     var fruitSelected = $("#fruit-selected").val();
     event.preventDefault();
-    console.log(fruitSelected);
     if (fruitSelected === "apples") {
       // change color of farm name using data in var arrays
     } else if (fruitSelected === "oranges") {
-      // change color of farm name using data in var arrays 
+      // change color of farm name using data in var arrays
     }
   });
 
