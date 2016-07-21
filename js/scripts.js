71a6cf460a4ef9086584c4bfe92f1610d736ba6f
Shelby Clayton and Zach Holz <shelby@>
2016-07-20 19:47:37 -0700

    Collect user input from dropdown

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,20 @@ var sauvieLavenderFruit = ["apples", "oranges"];
 
 
 
+// user interface
 $(document).ready(function() {
+  $("form#searchable").submit(function(event) {
+    var fruitSelected = $("#fruit-selected").val();
+    event.preventDefault();
+    console.log(fruitSelected);
+    if (fruitSelected === "apples") {
+      // change color of farm name using data in var arrays
+    } else if (fruitSelected === "oranges") {
+      // change color of farm name using data in var arrays 
+    }
+  });
+
+  // opens farm info (bug: for all farms)
   $(".farm-title").click(function() {
     $(".farm-info").toggle();
   });
