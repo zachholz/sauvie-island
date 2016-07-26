6413454208ace2a2e7f4206cc3af3db7063aacb9
Shelby Clayton <shelby_clayton@hotmail.com>
2016-07-25 20:13:52 -0700

    add variables for each farm with fruit listing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
 //var farms = {
 //    names: [columbiaFruit, douglasFruit, krugersFruit, pumpkinFruit, sauvieFarmFruit, sauvieBlueberryFruit, sauvieLavenderFruit]
 //}
-var farms(name, fruit, activities) = {
+var farm (name, fruit, activities) = {
   this.name = name;
   this.fruit = fruit;
   this.activities = activities;
 }
 
 $(document).ready(function() {
-  $("div.farm-title").click(function() {
-    $("div.farm-info").toggle();
+  $("div#douglas").click(function() {
+    $("div#douglas-info").toggle();
   });
 });
 
-var columbiaFruit = {fruits: ["apples", "oranges"]};
-var douglasFruit = {fruits: ["apples", "oranges"]};
-var krugersFruit = {fruits: ["apples", "oranges"]};
-var pumpkinFruit = {fruits: ["apples", "oranges"]};
-var sauvieBlueberryFruit = {fruits: ["apples"]};
-var sauvieFarmFruit = {fruits: ["oranges"]};
-var sauvieLavenderFruit = {fruits: ["apples"]};
+var columbia = new farm("Columbia Fruit", ["strawberries", "raspberries", "blackberries", "blueberries", "currants"], ["pumpkin patch", "u-pick", "sustainable farming"]);
+var douglas = new farm("Douglas Fruit", ["apples", "golden berries", "blackberries", "peaches", "marionberries", "boysenberries", "pears", "prunes"],[""]);
+var krugers = new farm("Kruger Farms", ["blueberries", "boysenberries", "raspberries", "marionberries"], [""]);
+var pumpkin = new farm("The Pumpkin Patch", ["pumpkins", "corn"], [""]);
+var sauvieBlueberry = new farm("Sauvie Island Blueberries", ["blueberries"], [""]);
+var sauvieFarm = new farm("Sauvie Island Farms"; ["peaches", "marionberries", "raspberries"], [""]);
+var sauvieLavender = new farm("Sauvie Island Lavender", [""], [""];
+
 
 // loop through farm names and find all those that contain fruit: apples
 var farmsWithApples = farms.map(function(name) {
